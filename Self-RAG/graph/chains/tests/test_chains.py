ba88db80c6d80f692232f5bc15d4a905acdbe874
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2025-05-16 00:00:23 +0530

    Add hallucination grader implementation and corresponding tests

--- Self-RAG/graph/chains/tests/test_chains.py
+++ Self-RAG/graph/chains/tests/test_chains.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 load_dotenv()
 from graph.chains.retriever_grader import GradeDocuments, retrieval_grader
 from graph.chains.generation import generation_chain
+from graph.chains.hallucination_grader import hallucination_grader , GradeHallucinations
 from ingestion import retriever
 from pprint import pprint
 
@@ -37,5 +38,29 @@ def test_generation_chain() -> None:
     )
 
     pprint(res) # pretty print
+
+def test_hallucination_grader_answer_yes() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+    generation = generation_chain.invoke(
+        {"question": question, "context": docs}
+    )
+    res: GradeHallucinations = hallucination_grader.invoke(
+        {"documents": docs, "generation": generation}
+    )
+
+    assert res.binary_score 
+
+def test_hallucination_grader_answer_no() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+    
+    res: GradeHallucinations = hallucination_grader.invoke(
+        {"documents": docs, "generation": "In order to make pizza we need to first start with the dough"}
+    )
+
+    assert not res.binary_score
+
+
 
     
