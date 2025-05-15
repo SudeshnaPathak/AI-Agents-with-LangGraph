7e3bde99bcceea5155d59dafe9b04af401fc44b0
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2025-05-16 00:54:41 +0530

    Implement grading for generation based on document grounding; update graph workflow

--- Self-RAG/graph/graph.py
+++ Self-RAG/graph/graph.py
@@ -2,7 +2,8 @@ from dotenv import load_dotenv
 from langgraph.graph import END , StateGraph
 from graph.consts import RETRIEVE , GENERATE , GRADE_DOCUMENTS , WEBSEARCH
 load_dotenv()
-
+from graph.chains.hallucination_grader import hallucination_grader
+from graph.chains.answer_grader import answer_grader
 from graph.nodes import generate , grade_documents , retrieve , web_search
 from graph.state import GraphState
 
@@ -17,6 +18,30 @@ def decide_to_generate(state):
     else:
         print("---DECISION: GENERATE---")
         return GENERATE
+    
+def grade_generation_grounded_in_documents_and_question(state: GraphState)-> str:
+    print("---CHECK HALLUCINATIONS---")
+    question = state["question"]
+    documents = state["documents"]
+    generation = state["generation"]
+    score = hallucination_grader.invoke(
+        {"documents": documents, "generation": generation}
+    )
+    if hallucination_grade := score.binary_score:
+        print("---DECISION: GENERATION IS GROUNDED IN DOCUMENTS---")
+        print("---GRADE GENERATION vs QUESTION---")
+        score = answer_grader.invoke(
+            {"question": question, "generation": generation}
+        )
+        if answer_grade := score.binary_score:
+            print("---DECISION: GENERATION ADDRESSES QUESTION---")
+            return "useful"
+        else:
+            print("---DECISION: GENERATION DOES NOT ADDRESS QUESTION---")
+            return "not useful"
+    else:
+        print("---DECISION: GENERATION IS NOT GROUNDED IN DOCUMENTS , RE-TRY---")
+        return "not suported"
     
 workflow = StateGraph(GraphState)
 #Add nodes to the workflow
@@ -37,12 +62,22 @@ workflow.add_conditional_edges(
     },
 )
 
+workflow.add_conditional_edges(
+    GENERATE,
+    grade_generation_grounded_in_documents_and_question,
+    {
+        "not supported": GENERATE,
+        "useful": END,
+        "not useful": WEBSEARCH,
+    },
+    )
+
 workflow.add_edge(WEBSEARCH , GENERATE)
 workflow.add_edge(GENERATE , END)
 
 app = workflow.compile()
-
 app.get_graph().draw_mermaid_png(output_file_path="graph.png")
+
 
 
 
