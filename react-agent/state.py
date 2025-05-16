682f6736526cc5c1aada0965e322cf04ec8aad0d
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2025-05-16 23:10:51 +0530

    Add AgentState class to manage agent state in the React agent

---
+++ react-agent/state.py
@@ -0,0 +1,12 @@
+import operator
+from typing import Annoted , TypedDict , Union
+from langchain_core.agents import AgentAction , AgentFinish
+
+class AgentState(TypedDict):
+    """
+    The state of the agent.
+    """
+    input: str
+    agent_outcome: Union[AgentAction, AgentFinish, None]
+    intermediate_steps: Annoted[list[tuple[AgentAction , str]] , operator.add] 
+    #type: list[tuple[AgentAction , str]] = [] , operator.add means that the list is mutable and can be modified in place
