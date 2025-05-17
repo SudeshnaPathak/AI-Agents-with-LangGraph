20ed3275fc75ec33ec3cf0c1b860e7bede79a869
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2025-05-17 09:29:32 +0530

    Add flow logic and graph generation to the React agent; update AgentState typing

--- react-agent/state.py
+++ react-agent/state.py
@@ -1,5 +1,5 @@
 import operator
-from typing import Annoted , TypedDict , Union
+from typing import Annotated , TypedDict , Union
 from langchain_core.agents import AgentAction , AgentFinish
 
 class AgentState(TypedDict):
@@ -8,5 +8,7 @@ class AgentState(TypedDict):
     """
     input: str
     agent_outcome: Union[AgentAction, AgentFinish, None]
-    intermediate_steps: Annoted[list[tuple[AgentAction , str]] , operator.add] 
+    intermediate_steps: Annotated[list[tuple[AgentAction , str]] , operator.add] 
     #type: list[tuple[AgentAction , str]] = [] , operator.add means that the list is mutable and can be modified in place
+    #AgentAction contains all details about which tool to use and what input to give it 
+    #AgentFinish contains the final output of the agent
